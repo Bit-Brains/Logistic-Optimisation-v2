03dbdce1967c23d1352cae1a619841ac6779d120
AbhishekBharati <210305105048@paruluniversity.ac.in>
2024-08-08 23:11:34 +0530

    Created Controller

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,8 +1,7 @@
 import express, { Request, Response } from "express";
+import { login } from "../controllers/authControllers"
 const router = express.Router();
 
-router.post("/login", (req: Request, res: Response) => {
-  res.send("hello World");
-})
+router.post("/login", login);
 
 export default router;
